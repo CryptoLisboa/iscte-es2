76ee4b264c35c1cc5512324f59135d8674e4b6b8
Bcfoo <>
2020-03-03 00:52:47 +0000

    + Added JUnit test + Fixed app buldPath property

---
+++ src/HelloWorld.java
@@ -0,0 +1,17 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class HelloWorld {
+	
+	@Test
+	public void testAdd1Plus1() 
+	{
+	    int y = 1;
+	    assertEquals(0, AnotherClass.add(true,y));
+	}
+
+	public static void main (String[] args) {
+		System.out.println("Tamos aqui !");
+	}
+}
