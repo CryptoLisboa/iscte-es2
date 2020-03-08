3367791dc5a99527fbd04e84f662843b11df007b
Ben-Hur Fidalgo <32733394+bcfoo-iscteiulpt@users.noreply.github.com>
2020-03-08 04:31:09 +0000

    Update HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,15 +1,15 @@
 import static org.junit.Assert.assertEquals;
 
-import org.junit.Test;
+//import org.junit.Test;
 
 public class HelloWorld {
 	
-	@Test
-	public void testAdd1Plus1() 
-	{
-	    int y = 1;
-	    assertEquals(0, AnotherClass.add(true,y));
-	}
+//	@Test
+//	public void testAdd1Plus1() 
+//	{
+//	    int y = 1;
+//	    assertEquals(0, AnotherClass.add(true,y));
+//	}
 
 	public static void main (String[] args) {
 		System.out.println("Tamos aqui !");
